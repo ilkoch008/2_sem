7f800fd7120693977199a52e435ec16fc28f1cec
Ilya <ilkoch008@gmail.com>
2018-10-04 00:49:27 +0300

    linuh

---
+++ mywc/cmake-build-debug/CMakeFiles/mywc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mywc.dir/main.c.o"
+  "mywc.pdb"
+  "mywc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mywc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
