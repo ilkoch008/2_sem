cb09d824fec81faef22bd16002cc16123b1f62bc
Ilya <ilkoch008@gmail.com>
2018-10-03 19:54:52 +0300

    linuh

---
+++ user_env/cmake-build-debug/CMakeFiles/test_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_1.dir/main.c.o"
+  "test_1.pdb"
+  "test_1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ user_env/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char **argv, char **envp)
+{
+    for (char **env = envp; *env != 0; env++)
+    {
+        char *thisEnv = *env;
+        if (argc > 0) {
+            if(strstr(thisEnv, argv[1]))
+                printf("%s\n", thisEnv);
+        }
+    }
+    return 0;
+}
