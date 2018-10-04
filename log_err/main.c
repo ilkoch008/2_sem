5233b25d1c95b4e3b07dd6b8e2a9ecb197b247b8
Ilya <ilkoch008@gmail.com>
2018-10-04 03:14:47 +0300

    linuh

--- log_err/main.c
+++ log_err/main.c
@@ -2,20 +2,40 @@
 #include <stdlib.h>
 #include <string.h>
 
+int numb;
+
+
+void url_nulling(char * str, int lenght){
+    strcpy(str, "wget ");
+    for(int i = 5; i < lenght; i++){
+        str[i] = '\0';
+    }
+}
+
+
 int main(int argc, char **argv, char **envp) {
-    FILE * texxt;
-    texxt = fopen(argv[1], "r+");
-    int nstr = 0, nwor = 0;
-    char * sttr = malloc(1000 * sizeof(char));
+    numb = 0;
+    FILE * logg;
+    logg = fopen(argv[1], "r+");
+    FILE * mylog;
+    mylog = fopen("my_log.log", "w+");
+    char * sttr = malloc(2000 * sizeof(char));
     char * wo;
-    while (fgets(sttr, 995, texxt) != NULL){
-        nstr++;
-        wo = strtok(sttr, " ,.;?!:");
-        while (wo != 0){
-            nwor++;
-            wo = strtok(NULL, " ,.;?!:\n");
+    char * fullurl = malloc(500 * sizeof(char));
+    while (fgets(sttr, 1995, logg) != NULL){
+        char * uurl = strstr(sttr, "http://");
+        if(strstr(sttr, "ERROR") != NULL && uurl != NULL){
+            url_nulling(fullurl, 499);
+            strncpy(fullurl+5, uurl, strcspn(uurl, " "));
+            while (uurl != NULL){
+                system(fullurl);
+                numb++;
+                url_nulling(fullurl, 499);
+                uurl = strstr(uurl+5, "http://");
+                if(uurl != NULL)
+                    strncpy(fullurl+5, uurl, strcspn(uurl, " "));
+            }
         }
     }
-    printf("strs: %d\nwords: %d\n", nstr, nwor);
     return 0;
 }
