f1ac323533cbd56798781132b7e3fd93b1685d94
Ilya <ilkoch008@gmail.com>
2018-11-29 15:29:00 +0300

    chat with backdoor added

---
+++ backdoor/clientachok/cmake-build-debug/CMakeFiles/clientachok.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clientachok.dir/main.c.o"
+  "clientachok.pdb"
+  "clientachok"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/clientachok.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ backdoor/servachok/cmake-build-debug/CMakeFiles/servachok.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/servachok.dir/main.c.o"
+  "servachok.pdb"
+  "servachok"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/servachok.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
